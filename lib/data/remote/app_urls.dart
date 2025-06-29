b2065cda88879d26ae115985cd4aeb5d45c7e0b1
Tushar Goyal <tushargoyal8096@gmail.com>
2025-06-29 10:06:48 +0530

    Delete lib/data/remote/app_urls.dart

--- lib/data/remote/app_urls.dart
+++
@@ -1,4 +0,0 @@
-class AppUrls{
-  static const String API_KEY = "AIzaSyDCVg0HmawpTVKvPlmbf0DTBZ1MD1wWr6I";
-  static const String baseURl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=$API_KEY";
-}
